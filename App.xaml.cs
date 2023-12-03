4dcef6804b795a3bb0394471aebb7ea460b389fd
jzmsm <jzmsm@DESKTOP-N89BK3J>
2023-12-03 08:09:10 -0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FreeNotes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
